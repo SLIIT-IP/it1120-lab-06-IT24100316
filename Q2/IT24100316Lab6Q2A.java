ee87f2b74b32babbe66ec8b24d60fb4b9b5be7c1
IT24100316 <IT24100316@my.sliit.lk>
2024-09-02 23:28:58 +0530

    Add files via upload

---
+++ Q2/IT24100316Lab6Q2A.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class IT24100316Lab6Q2A {
+    public static void main(String[] args) {
+         //create scanner
+        Scanner get = new Scanner(System.in);
+
+        //declare variables
+        int count = 1;
+
+        //loop
+        while(count  <=10){
+            System.out.print(count+" ");
+            count = count+1;
+        }
+    }
+}
